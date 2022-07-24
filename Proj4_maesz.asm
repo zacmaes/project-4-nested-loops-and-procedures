1906b9d1f99be6778eb29921900f96ae0a1bc474
zacmaes <zachary.maes@gmail.com>
2022-07-23 19:43:44 -0700

    Commit 1b: Added descriptions, comments, pseuodcode, and began calling procedures.

--- Proj4_maesz.asm
+++ Proj4_maesz.asm
@@ -1,4 +1,4 @@
-﻿TITLE Project Four     (Proj4_maesz.asm)
+﻿TITLE Project Four Nested Loops & Procedures     (Proj4_maesz.asm)
 
 ; Author: Zachary Maes
 ; Last Modified: July 23, 2022
@@ -43,14 +43,60 @@ INCLUDE Irvine32.inc
 
 ; (insert variable definitions here)
 
+; Introduction Data
+; 1. The programmer’s name and program title must appear in the output.
+intro_1 BYTE	"Welcome to Project Four Nested Loops & Procedures by Zachary Maes!",0
+
+; Directions Data
+direction_1 BYTE	"Directions: ",0
+direction_2 BYTE	"This program calculates and displays all of the prime numbers up to and including the nth prime.",0
+direction_3 BYTE	"In a moment, this program will have you enter the number (n) of prime numbers to be displayed.",0
+direction_4 BYTE	"The number you enter must be an integer in the range of [1 to 200] inclusive. ",0
+direction_5 BYTE	"If you enter a wrong number, the program will reprompt you to enter another number (hopefully correct this time around)",0
+direction_6 BYTE	"The results will be displayed 10 prime numbers per line, in ascending order, with at least 3 spaces between the numbers. ",0
+direction_7 BYTE	"The final row may contain fewer than 10 values.",0
+
+; GET USER DATA MESSAGE
+get_user_input_1 BYTE	"Enter the number of prime numbers would you like to print [1 to 200]: ",0
+
+; 7. If the user enters a number outside the range [1 ... 200] an error message must be displayed and the user must be prompted to re-enter the number of primes to be shown.
+; USER INPUT ERROR MESSAGE
+error_1 BYTE	"ERROR!",0
+error_2 BYTE	"You entered an invalid number. Please Try Again...",0
+;	after this, display get_user_input_1 message again.
+
 .code
 main PROC
 
 ; (insert executable instructions here)
 
+	call introduction
+    call getUserData
+		; call validate??? ---  where do I do it?
+    call showPrimes
+		; isPrime??? --- where?
+	call farewell
+
 	Invoke ExitProcess,0	; exit to operating system
 main ENDP
 
 ; (insert additional procedures here)
+
+introduction PROC
+
+
+	ret
+introduction ENDP
+
+getUserData PROC
+	
+	; call validate??? ---  where do I do it?
+	call validate
+
+
+	ret
+getUserData ENDP
+
+
 
 END main
